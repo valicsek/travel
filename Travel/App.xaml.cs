e94554ddc8efce59df8f4c8c963b17fc30bfb71b
Valicsek David <entriek@gmail.com>
2018-08-29 00:37:54 +0200

    Use NavigationPage for App

--- Travel/App.xaml.cs
+++ Travel/App.xaml.cs
@@ -11,7 +11,7 @@ namespace Travel
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
