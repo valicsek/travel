22964b0f540aa7d1bf900c65b017f829e65467c7
Valicsek David <entriek@gmail.com>
2018-08-29 00:29:28 +0200

    implement basic layout for login page

--- Travel/MainPage.xaml.cs
+++ Travel/MainPage.xaml.cs
@@ -13,5 +13,20 @@ namespace Travel
         {
             InitializeComponent();
         }
+
+        void OnButtonLogInClicked(object sender, System.EventArgs e)
+        {
+            if ( !string.IsNullOrEmpty(UsernameEntry.Text) && !string.IsNullOrEmpty(PasswordEntry.Text))
+            {
+                if( UsernameEntry.Text == "test")
+                {
+                    throw new NotImplementedException();
+                }
+            }
+            else
+            {
+                DisplayAlert("Alert", "Username or password is missing!", "OK");
+            }
+        }
     }
 }
