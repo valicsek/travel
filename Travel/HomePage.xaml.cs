599e1a501d2930852b552b50814698a4dec12dde
Valicsek David <entriek@gmail.com>
2018-08-29 10:07:38 +0200

    Install SQLite packages

---
+++ Travel/HomePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Travel
+{
+    public partial class HomePage : TabbedPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
